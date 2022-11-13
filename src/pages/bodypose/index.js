f712a108150e90c3f4949a74a4259610404b519f
Ted - TedfordMedia <ted@tedfordmedia.com>
2022-11-13 06:16:00 +0000

    quickfixes

--- src/pages/bodypose/index.js
+++ src/pages/bodypose/index.js
@@ -1,19 +1,16 @@
 import * as handpose from "@tensorflow-models/handpose";
-import Webcam from "react-webcam"; 
-import React, { Suspense,  useEffect, useCallback, useState } from 'react'
-import { Canvas, useTexture, useFrame } from '@react-three/fiber' 
- 
-import Layout from "../../components/layoutwidellh"  
- 
-function MyPage(props){
- 
-  
- 
+import Webcam from "react-webcam";
+import React, { Suspense, useEffect, useCallback, useState } from "react";
+import { Canvas, useTexture, useFrame } from "@react-three/fiber";
+
+import Layout from "../../components/layoutwidellh";
+
+function MyPage(props) {
   return (
-    <Layout>    
-         
-<div>asd</div>
-    </Layout> 
-)}
-  
-export default MyPage  
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+}
+
+export default MyPage;
