2d83e927393ab74d766da9938539f2a4c95e010b
Ted - TedfordMedia <78085953+TedfordMedia@users.noreply.github.com>
2022-11-13 05:32:40 +0000

    V2try2 (#2)
    * Index button, no pf word
    
    * Index button
    
    * about tedfordmedia and @components alias
    
    * misc
    
    * card updates

--- src/pages/portfolio_page/index.js
+++ src/pages/portfolio_page/index.js
@@ -1,85 +1,82 @@
-import React , {  useEffect } from 'react';
-import { graphql } from 'gatsby'
-import { Link } from "gatsby"
-import { css } from '@emotion/core'
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import Layout from "../../components/Layout"  
-import styled from '@emotion/styled'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../mystyling.scss'
-import { useTheme } from '../../components/Theming'
-import Image from 'gatsby-image' 
-import Header from '../../components/Header'
+import React, { useEffect } from "react";
+import { graphql } from "gatsby";
+import { Link } from "gatsby";
+import { css } from "@emotion/core";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import Layout from "@components/Layout";
+import styled from "@emotion/styled";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useTheme } from "@components/Theming";
+import Image from "gatsby-image";
 
-const StyledLink = styled(props => <Link {...props} />)`
-    color: black;
-    text-decoration: none;
+import "../mystyling.scss";
+const StyledLink = styled((props) => <Link {...props} />)`
+  color: black;
+  text-decoration: none;
 `;
 
 export default function Index({ data: { site, allMdx } }) {
-  const theme = useTheme()
- 
-  useEffect(() => {   
-      document.body.style.cursor = "auto";
-  }) 
+  const theme = useTheme();
+
+  useEffect(() => {
+    document.body.style.cursor = "auto";
+  });
   //
 
   return (
-    <Layout noSubscribeForm site={site}>  
-        {/* <Hero /> */}
-
-        <Container> 
-            <div className="portfolioheader">
-                <h2   css={css`
-                  width: 100%;
-                  flex-shrink: 0;
-                  background: none;
-                  padding: 20px 0;
-                  padding-bottom: 10px;
-                  font-weight: bold;
-                  font-size: 38px;
-                  color: ${theme.colors.headerBg};
-                `}>
-                  Portfolio
-                </h2>   
-            </div>
-            <Row>  
-            {allMdx.edges.map(({ node: post }) => ( 
-                <Col key={post.id} lg={6}> 
-                    <Card className="mycardpart mb-2 mt-2"> 
-                        <Card.Body className="mycardbody">
-                            {post.frontmatter.banner && (
-                                <Link
-                                    aria-label={`View ${post.frontmatter.title} article`}
-                                    to={`/${post.frontmatter.goto}`}
-                                    >
-                                    <Image   
-                                        className="zthecard_image" 
-                                        fluid={post.frontmatter.banner.childImageSharp.fluid}  
-                                    />
-                                </Link>
-                            )}
-                            <Card.Title className="mycardbottombit mytoppadding">
-                                <StyledLink
-                                    to={'../'+post.frontmatter.goto}
-                                    aria-label={`View ${post.frontmatter.title}`}
-                                >
-                                  {post.frontmatter.title}
-                                </StyledLink>
-                            </Card.Title>
-                            <Card.Text className="mycardbottombit  ">
-                                {post.frontmatter.description}{' '}
-                            </Card.Text>
-                        </Card.Body> 
-                    </Card>    
-                </Col>   
-            ))}
-        </Row> 
-        </Container>
+    <Layout noSubscribeForm site={site}>
+      <Container>
+        <div className="portfolioheader" />
+        <Row>
+          {allMdx.edges.map(({ node: post }) => (
+            <Col key={post.id} lg={6}>
+              <Card
+                className="mb-2 mt-2 mycardpart"
+                css={css`
+                  border-radius: 15px;
+                  padding: 0px;
+                  background-color: #edf2f6;
+                `}
+              >
+                <Card.Body
+                  className="mycardbody"
+                  css={css`
+                    border-radius: 15px;
+                    padding: 0px;
+                  `}
+                >
+                  {post.frontmatter.banner && (
+                    <Link
+                      aria-label={`View ${post.frontmatter.title} article`}
+                      to={`/${post.frontmatter.goto}`}
+                    >
+                      <Image
+                        className="zthecard_image"
+                        fluid={post.frontmatter.banner.childImageSharp.fluid}
+                      />
+                    </Link>
+                  )}
+                  <Card.Title className="mycardbottombit mytoppadding">
+                    <StyledLink
+                      to={"../" + post.frontmatter.goto}
+                      aria-label={`View ${post.frontmatter.title}`}
+                    >
+                      {post.frontmatter.title}
+                    </StyledLink>
+                  </Card.Title>
+                  <Card.Text className="mycardbottombit  ">
+                    {post.frontmatter.description}{" "}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </Layout>
-  )
+  );
 }
- 
+
 export const pageQuery = graphql`
   query {
     site {
@@ -94,7 +91,7 @@ export const pageQuery = graphql`
       filter: { frontmatter: { goto: { ne: null } } }
     ) {
       edges {
-        node { 
+        node {
           id
           fields {
             title
@@ -113,10 +110,10 @@ export const pageQuery = graphql`
             banner {
               childImageSharp {
                 fluid(maxWidth: 2000, maxHeight: 1300, quality: 100) {
-                    ...GatsbyImageSharpFluid
-                }    
+                  ...GatsbyImageSharpFluid
+                }
                 fixed(width: 300, height: 300) {
-                    ...GatsbyImageSharpFixed
+                  ...GatsbyImageSharpFixed
                 }
               }
             }
@@ -128,4 +125,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
