4af654d58ca754a1d0761ca40c8ab25ae291aec0
Ted - TedfordMedia <ted@tedfordmedia.com>
2022-11-18 03:18:50 +0000

    shaders22

--- src/pages/shaderone/index.js
+++ src/pages/shaderone/index.js
@@ -11,11 +11,6 @@ const MyPage = (props) => (
       <div  style={{ height: "100vh", width: "100%",background:"#a0a0a0" }}>  
         <Canvas    
           onCreated={({ gl, camera, scene }) => {  
-                // gl.outputEncoding = THREE.sRGBEncoding
-                // gl.shadowMap.enabled = true;
-                // gl.shadowMap.type = THREE.PCFSoftShadowMap; 
-                // const fogColor = new THREE.Color(0x000000);
-                // scene.fog = new THREE.Fog(fogColor, .0025, 80);
                 scene.background = new THREE.Color( 0x000000 ); 
             }}
             camera={{ 
@@ -23,7 +18,6 @@ const MyPage = (props) => (
             style={{ height: "100%", width: "100%" }}>
   
           <pointLight position={[-10, 20, 20]} intensity={1}/>
-          {/* <pointLight position={[10, -10, -10]} intensity={1}/> */}
           <Stars/> 
           <Suspense fallback={null}>   
               <LogoTedfordMedia position={[0, 1, -2]} scale={[20, 20, 20]} rotation={[90, 0, 0]} castShadow/>  
