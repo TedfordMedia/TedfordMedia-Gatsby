1dc4a5885edca13d565fa63813a4b9d4d6c6c7de
Ted - TedfordMedia <ted@tedfordmedia.com>
2022-12-13 15:56:48 +0000

    physics

---
+++ src/components/Stairs/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, Suspense } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import GenTools from "/src/GenTools";
+import { useLoader } from "@react-three/fiber";
+import useSceneStore from "/src/useSceneStore";
+
+function Stairs() {
+    const { scene } = useLoader(GLTFLoader, "/models/stairs.glb");
+    const exMetal = useSceneStore((state) => state.exMetal);
+
+    useEffect(() => {
+        GenTools.basicTraverse(scene);
+    }, [scene]);
+
+    useEffect(() => {
+        GenTools.applyGlass(scene);
+    }, [scene, exMetal]);
+
+    return (
+        <Suspense fallback={null}>
+            <primitive scale={[0.1, 0.1, 0.1]} object={scene} />
+        </Suspense>
+    );
+}
+
+export default Stairs;
