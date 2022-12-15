c83c978fd2f39ad703762d923f42fff2c0ed1b0b
Ted - TedfordMedia <78085953+TedfordMedia@users.noreply.github.com>
2022-12-15 19:01:59 +0000

    updates

---
+++ src/pages/videotest/index.js
@@ -0,0 +1,70 @@
+import React, { Suspense } from "react";
+import Layout from "../../components/layoutwide"
+import RobotStarsScene from "../../components/basics/robots/robotscenea"
+
+import { Canvas } from "@react-three/fiber";
+import * as THREE from 'three'
+import { Html, OrbitControls, useAspect, useVideoTexture, useTexture } from '@react-three/drei';
+
+function Scene() {
+  const size = useAspect(1800, 1000)
+  return (
+    <mesh scale={size}>
+      <planeGeometry />
+      <Suspense fallback={<FallbackMaterial url="/static/images/squareA.png" />}>
+        <VideoMaterial url="tedmediawelcomeai2.mp4" />
+      </Suspense>
+    </mesh>
+  )
+}
+
+function VideoMaterial({ url }) {
+  const texture = useVideoTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
+
+function FallbackMaterial({ url }) {
+  const texture = useTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
+
+const IndexPage = (props) => (
+  <Layout displayHero={false}>
+    <section >
+      <Canvas
+        style={{ height: "100%", width: "100%" }}
+        shadowMap
+        shadows
+        gl={{ alpha: false }}
+        camera={{
+          position: [0, .6, .7],
+          fov: 30,
+          near: 0.01,
+          far: 3000,
+        }}
+        onCreated={({ gl, camera, scene }) => {
+          gl.outputEncoding = THREE.sRGBEncoding;
+          gl.shadowMap.enabled = true;
+          gl.shadowMap.type = THREE.PCFSoftShadowMap;
+          gl.shadowMap.autoUpdate = true;
+          gl.toneMapping = THREE.ACESFilmicToneMapping;
+
+          const fogColor = new THREE.Color(0xffffff);
+          scene.background = fogColor;
+          // scene.fog = new THREE.Fog(fogColor, 0.0025, 80);
+          gl.setPixelRatio(window.devicePixelRatio);
+        }}
+      >
+        {/* <SimpleLighting /> */}
+        {/* <Floor /> */}
+
+        <Suspense fallback={<Html>loading</Html>}>
+          <Scene />
+        </Suspense>
+        <OrbitControls target={[0, 0, 0]} enableDamping dampingFactor={0.05} />
+      </Canvas>
+    </section>
+  </Layout>
+)
+
+export default IndexPage  
