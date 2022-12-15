73f0943afeb52e66c82dc770fbc2f1f7c421cc45
Ted - TedfordMedia <78085953+TedfordMedia@users.noreply.github.com>
2022-12-15 18:50:02 +0000

    updates

--- src/pages/singlecoupleroomc/index.js
+++ src/pages/singlecoupleroomc/index.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Html } from "@react-three/drei";
 import { css } from "@emotion/core";
-import EnvSingleCouple from "@helpers/Env_SwankyOffice5.js";
+import EnvSingleCouple from "@helpers/Lightroom2.js";
 import Layout from "@components/layoutwidellh";
 
 const MyPage = (props) => (
