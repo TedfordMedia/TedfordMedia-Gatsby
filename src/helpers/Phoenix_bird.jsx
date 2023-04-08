29c6a260facbf7176d872ef7edb9dd8acc2268a0
Ted - TedfordMedia <ted@tedfordmedia.com>
2023-04-08 17:46:45 +0100

    x

--- src/helpers/Phoenix_bird.jsx
+++ src/helpers/Phoenix_bird.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, Suspense } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
 import * as THREE from "three";
 
