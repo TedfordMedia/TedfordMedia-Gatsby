57f5cb673914977466009cc982f6c5cc9cb31ad7
Ted - TedfordMedia <ted@tedfordmedia.com>
2023-04-08 23:59:35 +0100

    s

--- src/helpers/Phoenix_bird.jsx
+++ src/helpers/Phoenix_bird.jsx
@@ -3,7 +3,7 @@ import { useGLTF, useAnimations, Html } from '@react-three/drei'
 import * as THREE from "three";
 
 export default function Bird(props) {
-  // const birdRef = useRef()
+  const birdRef = useRef()
   const { nodes, materials, animations } = useGLTF('/models/phoenix_bird.glb')
   // const { actions } = useAnimations(animations, birdRef)
 
@@ -24,25 +24,25 @@ export default function Bird(props) {
   return (
     <>
       <Suspense fallback={<Html>loading</Html>}>
-    //   <group ref={birdRef} {...props} dispose={null} >
-    //     <group name="Sketchfab_Scene" position={[0, 20, 0]} rotation={[0, -Math.PI / 2, 0]}>
-    //       <group name="Sketchfab_model" position={[-0.62, 0, -17.14]} rotation={[-Math.PI / 2, 0, 0.05]}>
-    //         <group name="5f59736c86d4457fa045aec4aea6b7e0fbx" rotation={[Math.PI / 2, 0, 0]}>
-    //           <group name="Object_2">
-    //             <group name="RootNode">
-    //               <group name="Object_4">
-    //                 <primitive object={nodes._rootJoint} />
-    //                 <group name="Object_6" rotation={[-Math.PI / 2, 0, 0]} />
-    //                 <group name="AMesh_Ride_FengHuang_01" rotation={[-Math.PI / 2, 0, 0]} />
-    //                 <skinnedMesh castShadow name="Object_7" geometry={nodes.Object_7.geometry} material={materials.MatI_Ride_FengHuang_01a} skeleton={nodes.Object_7.skeleton} />
-    //                 <skinnedMesh castShadow name="Object_8" geometry={nodes.Object_8.geometry} material={materials.MatI_Ride_FengHuang_01b} skeleton={nodes.Object_8.skeleton} />
-    //               </group>
-    //             </group>
-    //           </group>
-    //         </group>
-    //       </group>
-    //     </group>
-    //   </group>
+        <group ref={birdRef} {...props} dispose={null} >
+          {/* <group name="Sketchfab_Scene" position={[0, 20, 0]} rotation={[0, -Math.PI / 2, 0]}>
+          <group name="Sketchfab_model" position={[-0.62, 0, -17.14]} rotation={[-Math.PI / 2, 0, 0.05]}>
+            <group name="5f59736c86d4457fa045aec4aea6b7e0fbx" rotation={[Math.PI / 2, 0, 0]}>
+               <group name="Object_2">
+              <group name="RootNode">
+                 <group name="Object_4">
+                     <primitive object={nodes._rootJoint} />
+                   <group name="Object_6" rotation={[-Math.PI / 2, 0, 0]} />
+                    <group name="AMesh_Ride_FengHuang_01" rotation={[-Math.PI / 2, 0, 0]} />
+                   <skinnedMesh castShadow name="Object_7" geometry={nodes.Object_7.geometry} material={materials.MatI_Ride_FengHuang_01a} skeleton={nodes.Object_7.skeleton} />
+                    <skinnedMesh castShadow name="Object_8" geometry={nodes.Object_8.geometry} material={materials.MatI_Ride_FengHuang_01b} skeleton={nodes.Object_8.skeleton} />
+                </group>
+                </group>
+              </group>
+            </group>
+          </group>
+         </group> */}
+        </group>
       </Suspense>
     </>
   )
